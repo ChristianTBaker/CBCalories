f7ce09372823079a3850b84d7307058e683ca0e5
Christian Baker <Christian.Baker@echo.com>
2021-03-15 23:57:36 -0500

    init commit

---
+++ src/CBCalories.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using CBCalories.Core.ViewModels.Home;
+
+namespace CBCalories.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ src/CBCalories.UI/Resources/Colors.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CBCalories.UI.Resources
+{
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
